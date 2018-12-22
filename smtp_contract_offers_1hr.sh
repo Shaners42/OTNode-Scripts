dd52ef786742f010609f5adcf56d877551fb847c
Shaners42 <me@shanemolloy.ie>
2018-12-22 14:37:02 +0000

    Create smtp_contract_offers_1hr.sh

---
+++ smtp_contract_offers_1hr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#This script checks for OT-Node accepted offers within the last 24 hours
+#and sends an email using CURL to SendGrid's SMTP API. 
+#It runs outside Docker and looks at Docker logs (Container ID is statically set)
+#Disclaimer: I'm not a developer and everything here is very badly coded. It's just here so I can keep track. 
+
+gunzip /ot-node/logs/*.gz
+awk -v d="$(date -d'24 hours ago' +'{"log":"%Y-%m-%dT%k:%M:%S')" '$1" "$2>=d &&/ve been chosen for offer/' /var/lib/docker/containers/516*/516*-json.log > /home/output24hr.txt
+
+strCountOffers="Number of offers over the last 24 hours: "
+intCountOffers=$(cat /home/output24hr.txt | wc -l)
+strFullOffers=$strCountOffers$intCountOffers
+
+
+function sendOffersSMTP {
+        curl -i --request POST \
+                --url https://api.sendgrid.com/v3/mail/send \
+                --header 'Authorization: Bearer <API Key>' \
+                --header 'Content-Type: application/json' \
+                --data '{"personalizations": [{"to": [{"email": "<email address>"}]}],"from": {"email": "OTNode01@otnode01.ie"},"subject": 
+"OT Node Offers Daily" ,"content": [{"type": "text/plain", "value": "'"$strFullOffers"'"}]}'
+}
+
+if [[ $intCountOffers != 0 ]]
+then
+        sendOffersSMTP
+fi
